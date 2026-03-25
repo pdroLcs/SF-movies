7eaf8494efce8039dac8eb6ef8cb51e10772a188
Pedro Lucas <plfm3k@gmail.com>
2026-03-25 16:44:53 -0400

    Commit inicial

---
+++ src/main/java/br/com/pedro/sfmovies/SfMoviesApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.sfmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfMoviesApplication.class, args);
+    }
+
+}
