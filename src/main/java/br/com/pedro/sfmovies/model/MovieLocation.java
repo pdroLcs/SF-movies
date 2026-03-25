04d9f163902823571160b0697d2f16701ad63da5
Pedro Lucas <plfm3k@gmail.com>
2026-03-25 16:48:55 -0400

    feat: implementa model MovieLocation

---
+++ src/main/java/br/com/pedro/sfmovies/model/MovieLocation.java
@@ -0,0 +1,15 @@
+package br.com.pedro.sfmovies.model;
+
+import lombok.Data;
+
+@Data
+public class MovieLocation {
+
+    private String title;
+    private String release_year;
+    private String locations;
+    private String actor_1;
+    private String actor_2;
+    private String actor_3;
+
+}
