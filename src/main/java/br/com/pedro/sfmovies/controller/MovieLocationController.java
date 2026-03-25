725103e242f5019b7c809fa4ebe6cdd3da92c5ed
Pedro Lucas <plfm3k@gmail.com>
2026-03-25 17:23:05 -0400

    feat: implementa controller MovieLocationController

---
+++ src/main/java/br/com/pedro/sfmovies/controller/MovieLocationController.java
@@ -0,0 +1,27 @@
+package br.com.pedro.sfmovies.controller;
+
+import br.com.pedro.sfmovies.model.MovieLocation;
+import br.com.pedro.sfmovies.service.MovieLocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieLocationController {
+
+    private final MovieLocationService service;
+
+    @GetMapping
+    public List<MovieLocation> getAll(@RequestParam("t") Optional<String> title) {
+        return title.map(service::filterByTitle)
+                .orElseGet(service::getAllMovies);
+    }
+
+}
--- src/main/java/br/com/pedro/sfmovies/service/MovieLocationService.java
+++ src/main/java/br/com/pedro/sfmovies/service/MovieLocationService.java
@@ -1,4 +1,32 @@
 package br.com.pedro.sfmovies.service;
 
+import br.com.pedro.sfmovies.model.MovieLocation;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class MovieLocationService {
+
+    private final WebClient webClient;
+
+    public MovieLocationService(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("https://data.sfgov.org/resource/yitu-d5am.json").build();
+    }
+
+    public List<MovieLocation> getAllMovies() {
+        return webClient.get()
+                .retrieve()
+                .bodyToFlux(MovieLocation.class)
+                .collectList()
+                .block();
+    }
+
+    public List<MovieLocation> filterByTitle(String query) {
+        return getAllMovies().stream()
+                .filter(m -> m.getTitle() != null && m.getTitle().toLowerCase().contains(query.toLowerCase()))
+                .collect(Collectors.toList());
+    }
 }
